4e6074005c116317a6de5d2d5eb154d13b988469
Manoj Kumar Manyala <manojmanyala@gmail.com>
2018-10-31 22:36:08 +0530

    Tried to ad  update form to manager

--- Manager/urls.py
+++ Manager/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add_food/', views.add_food, name='add_food'),
     path('remove_food/', views.remove_food, name='remove_food'),
-    path('update_food/', views.update_food, name='update_food' )
+    path('update_food/', views.update_food, name='update_food'),
+    path('check_update_food/', views.check_update_food , name='check_update_food'),
 ]
