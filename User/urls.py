b9b71655ca38f5f58685a8b8c15e7d7b3b84115a
SatyaRaj <vineelraj19@gmail.com>
2018-11-08 17:19:39 +0530

    Change Homepage

--- User/urls.py
+++ User/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from User import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('', views.index, name=index),
-]
+    path('', views.index),
+]+static(settings.STATICS_URL, document_root=settings.STATIC_ROOT)
